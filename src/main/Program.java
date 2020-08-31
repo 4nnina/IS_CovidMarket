276d7a6f01cea1cf9a7c4370a429c488a20f0b4e
filippo.ziche <filippo.ziche@gmail.com>
2020-08-31 16:15:11 +0200

    Primo commit

---
+++ src/main/Program.java
@@ -0,0 +1,16 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Program extends Application
+{
+    public static void Main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
